ac744f4b1e701a6f6d8acbc1a551f10d8dc50815
DeepPasnani <168401187+DeepPasnani@users.noreply.github.com>
2026-01-09 05:21:59 +0000

    Write a 8085 program to subtract two 8-bit numbers stored in the registers.

--- practical2_3.asm
+++ practical2_3.asm
@@ -1,4 +1,4 @@
-;Practical 2_3 - Write a 8085 program to add two 8-bit numbers stored in the registers 
+;Practical 2_3 - Write a 8085 program to subtract two 8-bit numbers stored in the registers 
 MVI A,02H;
 MVI C,04h;
 SUB C;
